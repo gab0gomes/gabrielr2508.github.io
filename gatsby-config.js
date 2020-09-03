f0f59c80614d6c42f0792a25e4ab7d83d79a4693
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2020-09-03 12:29:30 -0300

    Limpa coisas desnecessárias do boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My blog`,
+    description: `A blog about my thoughts, front-end, agile and more cool stuffs.`,
+    author: `@gabrielr2508`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
