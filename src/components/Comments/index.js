03bff3353a0200cde43ded977d06265151acc617
Gabriel Rafael Gomes <gabrielrafael2508@hotmail.com>
2020-09-17 15:56:06 -0300

    Adiciona posts recomendados e comentarios

---
+++ src/components/Comments/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDisqusComments from 'react-disqus-comments';
+import propTypes from 'prop-types';
+
+import * as S from './styled';
+
+const Comments = ({ url, title }) => {
+  const completeURL = `https://gabrielr2508.github.com${url}`;
+  return (
+    <S.CommentsWrapper>
+      <S.CommentsTitle>
+        Comentários
+      </S.CommentsTitle>
+      <ReactDisqusComments
+        shortname="gabrielr2508"
+        identifier={ completeURL }
+        title={ title }
+        url={ completeURL }
+      />
+    </S.CommentsWrapper>
+  );
+};
+
+Comments.propTypes = {
+  url: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+};
+
+export default Comments;
