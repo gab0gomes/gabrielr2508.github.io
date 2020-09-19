0b3d04053c3045bc55fb3adbaeee23d6e1fdc336
Gabriel Rafael Gomes <gabrielrafael2508@hotmail.com>
2020-09-19 12:48:04 -0300

    Adiciona tema light

--- src/styles/global.js
+++ src/styles/global.js
@@ -59,8 +59,28 @@ const GlobalStyles = createGlobalStyle`
   }
   img {
     display: block;
-  	width: 100%;
-  	height: auto;
+    width: 100%;
+    height: auto;
+  }
+  body.light {
+    --borders: #dedede;
+    --postColor: #111;
+    --texts: #555555;
+    --highlight: #1fa1f2;
+    --mediumBackground: #f0f0f3;
+    --background: #fff;
+    --white: #fff;
+    --black: #222;
+  }
+  body.dark {
+    --borders: #38444d;
+    --texts: #8899a6;
+    --postColor: #fff;
+    --highlight: #1fa1f2;
+    --mediumBackground: #192734;
+    --background: #16202c;
+    --white: #fff;
+    --black: #222;
   }
 `;
 
