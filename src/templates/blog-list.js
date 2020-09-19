2c09748ccdc2255996e9310895d31ec23d3f00ba
Gabriel Rafael Gomes <gabrielrafael2508@hotmail.com>
2020-09-19 15:19:02 -0300

    Adiciona layout tipo grid

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -6,6 +6,8 @@ import SEO from '../components/seo';
 import PostItem from '../components/PostItem';
 import Pagination from '../components/Pagination';
 
+import * as S from '../components/ListWrapper/styled';
+
 const BlogList = (props) => {
   const postList = props.data.allMarkdownRemark.edges;
   const { currentPage, numPages } = props.pageContext;
@@ -20,35 +22,36 @@ const BlogList = (props) => {
   return (
     <Layout>
       <SEO title="Home" />
-
-      {
-        postList.map(({
-          node: {
-            fields: {
-              slug,
+      <S.ListWrapper>
+        {
+          postList.map(({
+            node: {
+              fields: {
+                slug,
+              },
+              frontmatter: {
+                background,
+                category,
+                date,
+                description,
+                title,
+              },
+              timeToRead,
             },
-            frontmatter: {
-              background,
-              category,
-              date,
-              description,
-              title,
-            },
-            timeToRead,
-          },
-        }) => (
-          <PostItem
-            slug={ slug }
-            background={ background }
-            category={ category }
-            date={ date }
-            timeToRead={ timeToRead }
-            title={ title }
-            description={ description }
-          />
+          }) => (
+            <PostItem
+              slug={ slug }
+              background={ background }
+              category={ category }
+              date={ date }
+              timeToRead={ timeToRead }
+              title={ title }
+              description={ description }
+            />
+          )
         )
-      )
-    }
+      }
+    </S.ListWrapper>
     <Pagination
       isFirst={isFirst}
       isLast={isLast}
